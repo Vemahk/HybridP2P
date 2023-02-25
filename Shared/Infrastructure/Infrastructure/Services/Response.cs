c9a7e8eb1194a579f0b3593715f35e2a0d0da938
Vemahk <vemahk20@gmail.com>
2023-02-25 16:54:17 -0600

    Infrastructure 0.0.1

---
+++ Shared/Infrastructure/Infrastructure/Services/Response.cs
@@ -0,0 +1,50 @@
+﻿namespace Infrastructure.Services;
+
+public class Response
+{
+    public bool Success { get; internal init; } = true;
+    public string? Message { get; internal init; }
+
+    public static Response Pass() => new Response { Success = true };
+    public static Response<T> Pass<T>(T data) => new(data);
+    public static FailureResponse Fail(string reason) => new(reason);
+    public static FailureResponse Fail(Response other) => new(other.Message);
+
+    public static implicit operator Response(FailureResponse failure)
+    {
+        return new Response
+        {
+            Success = false,
+            Message = failure.Message,
+        };
+    }
+}
+
+public sealed class Response<T> : Response
+{
+    public T Data { get; internal init; }
+
+    public Response(T data)
+    {
+        Data = data;
+    }
+
+    public static implicit operator Response<T>(FailureResponse failure)
+    {
+        return new Response<T>(default!)
+        {
+            Success = false,
+            Message = failure.Message,
+        };
+    }
+}
+
+public sealed class FailureResponse 
+{
+    internal FailureResponse(string? message)
+    {
+        Message = message;
+    }
+
+    public string? Message { get; }
+}
---
+++ Shared/Infrastructure/Infrastructure/Services/ServiceBase.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Services;
+
+public abstract class ServiceBase<TService>
+    where TService : ServiceBase<TService>
+{
+    protected readonly ILogger<TService> Logger;
+
+    protected ServiceBase(ILogger<TService> logger)
+    {
+        Logger = logger;
+    }
+
+    protected static Response Pass() => Response.Pass();
+    protected static Response<T> Pass<T>(T data) => Response.Pass(data);
+    protected static FailureResponse Fail(string reason) => Response.Fail(reason);
+    protected static FailureResponse Fail(Response other) => Response.Fail(other);
+
+    protected FailureResponse UnexpectedError(Exception e, string? message = null)
+    {
+        message ??= e.Message;
+        Logger.LogError("{message} {exception}", message, e);
+        return Fail(message ?? e.Message);
+    }
+}
